9778c2cc719c1925ff60cc9f58a2a65b9000f204
nstrucky <nickstruckmeyer@gmail.com>
2017-01-12 21:25:31 -0500

    Search is functional and loader persists data through orientation changes

--- app/src/main/java/com/netjob/booklisting/MainActivity.java
+++ app/src/main/java/com/netjob/booklisting/MainActivity.java
@@ -4,13 +4,18 @@ import android.app.LoaderManager;
 import android.content.AsyncTaskLoader;
 import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.net.ConnectivityManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -20,30 +25,64 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
 
 
-    ListView mBooksListView;
-    EditText mSearchEditText;
-    List<Book> mBooks;
-    BookAdapter mBookAdapter;
+    private ListView mBooksListView;
+    private EditText mSearchEditText;
+    private List<Book> mBooks;
+    private BookAdapter mBookAdapter;
+    private Button mSearchButton;
+    private static String mSearchTerms = "";
+    BookLoader bookLoader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSearchButton = (Button) findViewById(R.id.button_search);
         mSearchEditText = (EditText) findViewById(R.id.editText_searchBox);
+
+        mSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSearchTerms = mSearchEditText.getText().toString();
+                getLoaderManager().restartLoader(0, null, MainActivity.this);
+
+
+            }
+        });
 
         mBooks = new ArrayList<>();
         mBooksListView = (ListView) findViewById(R.id.listView_booksReturned);
         mBookAdapter = new BookAdapter(this, mBooks);
         mBooksListView.setAdapter(mBookAdapter);
-        getLoaderManager().initLoader(0, null, this);
+
+        startLoader();
 
     }
+
+
+    private void startLoader() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected()) {
+
+                getLoaderManager().initLoader(0, null, this);
+
+
+        } else {
+            Toast.makeText(this, getString(R.string.message_no_internet), Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
-        return new BookLoader(this);
+        bookLoader = new BookLoader(this);
+
+        return bookLoader;
     }
+
 
     @Override
     public void onLoadFinished(Loader<List<Book>> loader, List<Book> books) {
@@ -54,18 +93,18 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         }
     }
 
+
     @Override
     public void onLoaderReset(Loader<List<Book>> loader) {
+
 
     }
 
     private static class BookLoader extends AsyncTaskLoader<List<Book>> {
 
-
         public BookLoader(Context context) {
             super(context);
         }
-
 
         @Override
         protected void onStartLoading() {
@@ -76,7 +115,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         @Override
         public List<Book> loadInBackground() {
 
-            URL url = QueryUtils.buildUrl("The Godfather");
+            URL url = QueryUtils.buildUrl(mSearchTerms);
             return QueryUtils.makeHttpRequest(url);
 
         }
